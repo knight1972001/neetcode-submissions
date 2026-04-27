b65a985fab93b35f00bc6bd2a85f281a3820935e
Long Nguyen <60019805+knight1972001@users.noreply.github.com>
2026-04-26 22:35:19 -0400

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # arr -> set (unique)
+        num_set = set(nums)
+        longest = 0
+
+        # check if num - 1 in not in set == it means this number is smallest. we can start checking number greater
+        # if num - 1 in set, it means this number is not smallest. we can start with it later.
+        for num in num_set:
+            if (num-1) not in num_set:
+                length = 1
+        # checking greater number by that num + length must be in the set.
+                while (num+length) in num_set:
+                    length += 1
+        # compare longest and current length. get the max
+                longest = max(length, longest)
+        
+        return longest
+
+
+
+        
