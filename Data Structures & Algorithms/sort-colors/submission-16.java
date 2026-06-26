f98ff65a96eaeed16fc9c998c7790c49c1b813d7
Long Nguyen <60019805+knight1972001@users.noreply.github.com>
2026-06-25 21:30:47 -0400

    Add: sort-colors - submission-16

---
+++ Data Structures & Algorithms/sort-colors/submission-16.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int left = 0, i = 0, right = nums.length - 1;
+
+        while (i <= right){
+            if (nums[i] == 0){
+                swap(nums, i, left);
+                left++;
+            }else if (nums[i] == 2){
+                swap(nums, i, right);
+                right--;
+                i--;
+            }
+            i++;
+        }
+    }
+
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
