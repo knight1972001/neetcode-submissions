f89ee4138ba3464661d8e57f2b3a45c2cd7f4583
Long Nguyen <60019805+knight1972001@users.noreply.github.com>
2026-06-26 00:39:02 -0400

    Add: palindrome-number - submission-0

---
+++ Data Structures & Algorithms/palindrome-number/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        char[] chArr = Integer.toString(x).toCharArray();
+
+        int left = 0, right = chArr.length - 1;
+
+        while(left < right){
+            if(chArr[left] == chArr[right]){
+                left++;
+                right--;
+            }else{
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
