759edfe81b569fe624f4b4e7fb8927765a15e088
Long Nguyen <60019805+knight1972001@users.noreply.github.com>
2026-06-27 18:42:29 -0400

    Add: kth-largest-element-in-an-array - submission-10

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-10.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+    public int quickSelect(int[] nums, int left, int right, int k){
+        int p = left, pivot = nums[right];
+
+        for (int i = left; i < right; i++){
+            if (nums[i] < pivot){
+                swap(nums, i, p);
+                p++;
+            }
+        }
+
+        swap(nums, right, p);
+
+        if (k > p){
+            return quickSelect(nums, p + 1, right, k);
+        }else if (k < p){
+            return quickSelect(nums, left, p - 1, k);
+        }else{
+            return nums[k];
+        }
+    }
+
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
